85bf944bc7f7f0845c764e80e5303439ad6167d8
Dr.Mohmmed <mohmmed9770@gmail.com>
2023-07-09 21:45:37 +0200

    first commit

---
+++ pizza.js
@@ -0,0 +1,10 @@
+
+
+const check = document.getElementById('.check');
+const pay = document.getElementById('.pay');
+
+check.addEventListener('click', () => {
+  
+    pay.style.display = 'block';
+  
+});
